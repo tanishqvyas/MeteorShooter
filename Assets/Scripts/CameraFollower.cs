38b1755fba44734d32aa12d3735d0bc32c5e9d5b
tanishqvyas <tanishqvyas069@gmail.com>
2020-04-27 22:20:01 +0530

    Camera follower script added

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+
+	public GameObject SpaceShip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// Change camera's coordinates as per that os the ship
+    	transform.position = new Vector3(SpaceShip.transform.position.x, SpaceShip.transform.position.y, transform.position.z );
+    }
+}
