7f0ce7b6b92c87e0dafcc36d71896dc9b4d9eaa3
tanishqvyas <tanishqvyas069@gmail.com>
2020-04-28 11:46:14 +0530

    added shooting functionality

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+
+{
+    // public GameObject hitEffect;
+
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/BulletFiring.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletFiring : MonoBehaviour
+{
+
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get vector pos of gun point
+        // gunpointPos = gunpoint.transform.position;
+
+        // Subtract spaceship position vector from this to get direction vector of gun wrt ship
+        // Vector math bitch!!
+        // fireDirection = gunpointPos - rigidBody.position;
+        // float angle = Mathf.Atan2(fireDirection.y, fireDirection.x) * Mathf.Rad2Deg- 90f;
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+
+    void Shoot()
+    {
+        // Instantiate bullet
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        rb.AddForce(firepoint.up * bulletForce,ForceMode2D.Impulse);
+
+    }
+}
