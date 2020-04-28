37c8800e53f77ed8791ddd8121c038f49e19718d
tanishqvyas <tanishqvyas069@gmail.com>
2020-04-28 20:36:08 +0530

    Added random bg star spawner

---
+++ Assets/Scripts/SpawnStars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStars : MonoBehaviour
+{
+
+    public GameObject starPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-351f, 351f), Random.Range(-200f, 200f));
+
+            Instantiate(starPrefab, pos , Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
