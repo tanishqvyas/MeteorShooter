75c30fbc2e64b48d647c812febc94644b37b7c4c
tanishqvyas <tanishqvyas069@gmail.com>
2020-04-29 00:36:05 +0530

    Added asteroid destruction effect

--- Assets/Scripts/AsteroidBehaviour.cs
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AsteroidBehaviour : MonoBehaviour
 {
     public float lifeCount = 3f;
+    public GameObject destroyEffect;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,7 +16,10 @@ public class AsteroidBehaviour : MonoBehaviour
 
         if(lifeCount == 0)
         {
+            GameObject effect = Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
             Destroy(gameObject);
+
         }
     }
 }
