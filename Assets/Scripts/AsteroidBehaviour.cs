e98803dfa511571d8df58f737babcc3ba1b000b4
tanishqvyas <tanishqvyas069@gmail.com>
2020-05-01 21:03:34 +0530

    Asteroids spawn and destroy functionality added

--- Assets/Scripts/AsteroidBehaviour.cs
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -7,6 +7,11 @@ public class AsteroidBehaviour : MonoBehaviour
     public float lifeCount = 3f;
     public GameObject destroyEffect;
 
+    //Asteroids
+    public GameObject nextAsteroid;
+   
+    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "bullet")
@@ -15,10 +20,34 @@ public class AsteroidBehaviour : MonoBehaviour
         }
 
         if(lifeCount == 0)
-        {
-            GameObject effect = Instantiate(destroyEffect, transform.position, Quaternion.identity);
-            Destroy(effect, 1f);
+        {   
+            // Get the name
+            string asteroidName = gameObject.tag;
+
+            Debug.Log(asteroidName);
+            
+            // Destroy the current asteroid
             Destroy(gameObject);
+            
+            if(asteroidName != "asteroid1")
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    GameObject temp = Instantiate(nextAsteroid, transform.position, Quaternion.identity);
+                    if(Random.Range(-2f,2f) > 0)
+                        temp.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.right * Random.Range(1f,20f) * Random.Range(-2f, 2f),ForceMode2D.Impulse);
+                    else
+                        temp.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * Random.Range(1f,20f) * Random.Range(-2f, 2f),ForceMode2D.Impulse);
+                }
+            }
+
+            else
+            {
+                GameObject effect = Instantiate(destroyEffect, transform.position, Quaternion.identity);
+                Destroy(effect, 1f);
+            }
+
+
 
         }
     }
--- Assets/Scripts/SpawnStars.cs
+++ Assets/Scripts/SpawnStars.cs
@@ -6,6 +6,10 @@ public class SpawnStars : MonoBehaviour
 {
 
     public GameObject starPrefab;
+    public GameObject asteroid4;
+    public GameObject asteroid3;
+    public GameObject asteroid2;
+    public GameObject asteroid1;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,36 @@ public class SpawnStars : MonoBehaviour
 
             Instantiate(starPrefab, pos , Quaternion.identity);
         }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-42f, 850f), Random.Range(-120f, 120f));
+
+            Instantiate(asteroid4, pos , Quaternion.identity);
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-42f, 850f), Random.Range(-120f, 120f));
+
+            Instantiate(asteroid3, pos , Quaternion.identity);
+        }
+
+        for (int i = 0; i < 50; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-42f, 850f), Random.Range(-120f, 120f));
+
+            Instantiate(asteroid2, pos , Quaternion.identity);
+        }
+
+        for (int i = 0; i < 500; i++)
+        {
+            Vector2 pos = new Vector2(Random.Range(-42f, 850f), Random.Range(-120f, 120f));
+
+            Instantiate(asteroid1, pos , Quaternion.identity);
+        }
+
+
     }
 
     // Update is called once per frame
